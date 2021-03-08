403cccfffb1a047145d70af8bc63f78583dff4d3
PRANJAL KUMAR <pranjal.kumar.153@gmail.com>
2021-03-08 16:04:11 +0530

    removed front-end.

--- frontend/app.js
+++ frontend/app.js
@@ -2,10 +2,22 @@ var express = require("express");
 var app = express();
 
 var parser = require("body-parser");
-var nodemailer = require('nodemailer');
+
 var request = require("request");
 
 app.use(parser.urlencoded({ extended: true }));
+
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res) {
+    res.render("sign-in.ejs");
+});
+app.get("/signin", function(req, res) {
+    res.render("sign-in.ejs");
+});
+app.get("/signup", function(req, res) {
+    res.render("sign-up.ejs");
+});
 
 
 app.listen(8000, 8000, function(req, res) {
